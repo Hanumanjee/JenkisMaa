d76294d536bd1673f90279f1fe12f8304bc23e4d
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-06-10 11:29:59 -0700

    change build gradle

--- src/test/java/alltest/MyPersonalTest.java
+++ src/test/java/alltest/MyPersonalTest.java
@@ -1,11 +1,27 @@
 package alltest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class MyPersonalTest {
-    public static void main(String[] args) {
-        System.out.println("My personal branch");
-        /////
-        System.out.println("My personal branch");
-        System.out.println("My personal branch");
-        System.out.println("My personal branch");
+    WebDriver driver;
+
+    @BeforeMethod
+    public void startUp(){
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+    @Test
+    public void verifyJenkins(){
+        System.out.println("Maa Jenkins Testing");
+        System.out.println("Second changes");
     }
 }
