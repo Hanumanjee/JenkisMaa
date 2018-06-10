070119902fa8fc28d0010739e6c8ec87fa0605c7
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-06-10 09:37:16 -0700

    Added a package my personal branch1 //

--- src/test/java/alltest/MyPersonalTest.java
+++ src/test/java/alltest/MyPersonalTest.java
@@ -3,6 +3,9 @@ package alltest;
 public class MyPersonalTest {
     public static void main(String[] args) {
         System.out.println("My personal branch");
+        /////
+        System.out.println("My personal branch");
+        System.out.println("My personal branch");
         System.out.println("My personal branch");
     }
 }
