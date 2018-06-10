2b851dfad5dcb13c9c51fcc680fe66f0448816a9
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-06-10 09:33:47 -0700

    Added a package my personal branch1

--- src/test/java/alltest/MyPersonalTest.java
+++ src/test/java/alltest/MyPersonalTest.java
@@ -3,5 +3,6 @@ package alltest;
 public class MyPersonalTest {
     public static void main(String[] args) {
         System.out.println("My personal branch");
+        System.out.println("My personal branch");
     }
 }
