b1e52987204bb250c26eadb9b356537e72a4de5d
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-06-10 09:56:20 -0700

    Added a package my personal branch1 ////

--- src/test/java/testdata/HashMapTest.java
+++ src/test/java/testdata/HashMapTest.java
@@ -4,6 +4,7 @@ public class HashMapTest {
 
     public static void main(String[] args) {
         System.out.println("HashMapTest");
-        //
+        System.out.println("if it works i will mearge to master\n");
+        System.out.println("This should work");
     }
 }
