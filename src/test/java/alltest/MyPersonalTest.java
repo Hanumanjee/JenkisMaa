e58d2cb89e2c002c28df6d0edd0d8904ba664de4
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-06-17 13:35:27 -0700

    change build gradle added jenkinstestng.xml --11

--- src/test/java/alltest/MyPersonalTest.java
+++ src/test/java/alltest/MyPersonalTest.java
@@ -1,24 +1,12 @@
 package alltest;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MyPersonalTest {
     WebDriver driver;
 
-    @BeforeMethod
-    public void startUp(){
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-    }
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
-    }
+
     @Test
     public void verifyJenkins(){
         System.out.println("Maa Jenkins Testing");
