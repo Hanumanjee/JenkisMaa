dc1d391ab6a222b2fd7936a73bb888f5ce28e4db
Hanumanjee <39235122+Hanumanjee@users.noreply.github.com>
2018-06-17 13:34:23 -0700

    Update MyPersonalTest.java
    test

--- src/test/java/alltest/MyPersonalTest.java
+++ src/test/java/alltest/MyPersonalTest.java
@@ -9,16 +9,7 @@ import org.testng.annotations.Test;
 public class MyPersonalTest {
     WebDriver driver;
 
-    @BeforeMethod
-    public void startUp(){
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-    }
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
-    }
+    
     @Test
     public void verifyJenkins(){
         System.out.println("Maa Jenkins Testing");
